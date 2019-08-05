29c1985481a87048513b9b6f8c20c92cfbc7c239
madddmax <m_prokofiev@mail.ru>
2019-08-06 00:50:24 +0300

    Priority capture the enemy

--- Simulator.cs
+++ Simulator.cs
@@ -54,42 +54,52 @@ namespace MiniAiCupPaperio
 
             if (myNext.Lines.Any(l => l[0] == x && l[1] == y))
             {
+                // пересекает свой шлейф
                 return null;
             }
 
-            if (!myNext.Territory.Any(t => t[0] == x && t[1] == y))
+            var onMyTerritory = myNext.Territory.Any(t => t[0] == x && t[1] == y);
+            if (!onMyTerritory)
             {
-                // вне своей территорий
+                // вне своей территорий - увеличиваем шлейф
                 var lines = myNext.Lines.ToList();
                 lines.Add(myNext.Position);
                 myNext.Lines = lines.ToArray();
             }
-            else if (myNext.Lines.Length > 0)
-            {
-                // завершает шлейф на своей территории
-                myNext.Lines = new int[0][]; 
 
-                // todo считает шлейф, а надо шлейф + то что внутри
-                foreach (var l in my.Lines)
-                {
-                    myNext.Score += (Math.Abs(l[0] - x) + Math.Abs(l[1] - y)) / World.Width;
-                }
-            }
-
+            var allEnemyTerritory = new List<int[]>();
             foreach (var e in Enemies)
             {
-                if (depth >= 1 && myNext.Lines.Any(l =>
-                    Math.Abs(l[0] - e.Position[0]) + Math.Abs(l[1] - e.Position[1]) <= World.Width * (depth + 2))) // depth + 5
+                if (depth >= 1 && 
+                    myNext.Lines.Any(l => Math.Abs(l[0] - e.Position[0]) + Math.Abs(l[1] - e.Position[1]) <= World.Width * (depth + 2)))
                 {
-                    // страх перерезания шлейфа
+                    // страх пересечения шлейфа
                     return null;
                 }
 
                 if (e.Lines.Any(l => l[0] == x && l[1] == y))
                 {
-                    // перерезает шлейф противника
+                    // пересекает шлейф противника
                     myNext.Score += 50;
                 }
+
+                allEnemyTerritory.AddRange(e.Territory);
+            }
+
+            if (onMyTerritory && myNext.Lines.Length > 0)
+            {
+                // завершает шлейф на своей территории
+                foreach (var l in myNext.Lines)
+                {
+                    int captureScore = 1;
+                    if (allEnemyTerritory.Any(t => t[0] == l[0] && t[1] == l[1]))
+                    {
+                        captureScore = 5;
+                    }
+                    myNext.Score += ((Math.Abs(l[0] - x) + Math.Abs(l[1] - y)) / World.Width) * captureScore;
+                }
+
+                myNext.Lines = new int[0][];
             }
 
             return myNext;
