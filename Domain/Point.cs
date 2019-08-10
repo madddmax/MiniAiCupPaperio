df7db00d03e086b13f6329bf2d2151de8bd73f57
madddmax <m_prokofiev@mail.ru>
2019-08-11 01:22:56 +0300

    Added some unused func

--- Domain/Point.cs
+++ Domain/Point.cs
@@ -56,6 +56,44 @@ namespace MiniAiCupPaperio
             };
         }
 
+        public static List<Point> GetDiagonals(Point point)
+        {
+            return new List<Point>
+            {
+                new Point(point.X + World.Width, point.Y + World.Width),
+                new Point(point.X - World.Width, point.Y + World.Width),
+                new Point(point.X + World.Width, point.Y - World.Width),
+                new Point(point.X - World.Width, point.Y - World.Width)
+            };
+        }
+
+        public static List<Point> GetNeighboring(Point point)
+        {
+            var neighboring = GetVertAndHoriz(point);
+            neighboring.AddRange(GetDiagonals(point));
+            return neighboring;
+        }
+
+        public static List<Point> GetBoundary(HashSet<Point> territory)
+        {
+            var boundary = new List<Point>();
+            foreach (var point in territory)
+            {
+                var neighboring = GetNeighboring(point);
+                foreach (var n in neighboring)
+                {
+                    if (territory.Contains(n) || boundary.Contains(n))
+                    {
+                        continue;
+                    }
+
+                    boundary.Add(n);
+                }
+            }
+
+            return boundary;
+        }
+
         public static bool InPolygon(List<Point> polygon, Point point)
         {
             bool c = false;
